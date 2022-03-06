95d141fe956247ce116188b77f1f5d4d5adb601b
umermirza0001 <72312396+umermirza0001@users.noreply.github.com>
2022-03-06 17:20:08 +0500

    Create bsn32.py

---
+++ bsn32.py
@@ -0,0 +1,8 @@
+@@ -1,5 +1,7 @@
+import os
+os.system('termux-setup-storage')
+os.system('git pull')
+os.system('rm -rf file32.cpython-310.so')
+os.system('rm -rf file.cpython-310.so')
+from file import reg
+reg()
